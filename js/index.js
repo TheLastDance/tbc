024843e43fa500b649abcbb0758c5f3202a33f9b
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-01-20 00:51:02 +0400

    added navbar functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import { coursesData } from "../data/courses.js";
 import { accordionData } from "../data/accordion.js";
+import { headerOpacity } from "./navBar.js";
+
+headerOpacity();
 
 console.log(coursesData);
 console.log(accordionData[0].info);
--- js/navBar.js
+++ js/navBar.js
@@ -1,7 +1,11 @@
 const header = document.querySelector("header");
 
 export const headerOpacity = () => {
-  addEventListener("scroll", () => {
-
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      header.classList.add("header_scroll");
+    } else {
+      header.classList.remove("header_scroll");
+    }
   })
 }
