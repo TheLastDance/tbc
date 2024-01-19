f9f16beefc156590ac370f7d1f4f017fbe26a08a
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-01-19 21:29:48 +0400

    base structure

--- data/courses.js
+++ data/courses.js
@@ -1,7 +1,47 @@
 export const coursesData = [
   {
-    name: "",
-    reg: "",
-    photo: "../assets/img/001.jpg",
+    name: "iOS Development",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/01.webp",
+  },
+  {
+    name: "React",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/02.webp",
+  },
+  {
+    name: "Intro to Python",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/03.webp",
+  },
+  {
+    name: "Advanced Python",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/04.webp",
+  },
+  {
+    name: "Advanced Cybersecurity Course",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/05.webp",
+  },
+  {
+    name: "ToT - Training of Trainers",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/06.webp",
+  },
+  {
+    name: "Blockchain",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/07.webp",
+  },
+  {
+    name: "DevOps",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/08.webp",
+  },
+  {
+    name: "Information Security Governance",
+    reg: "რეგისტრაცია დასრულებულია",
+    photo: "../assets/img/courses/09.webp",
   }
 ]
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 import { coursesData } from "../data/courses.js";
+import { accordionData } from "../data/accordion.js";
 
 console.log(coursesData);
+console.log(accordionData[0].info);
