2a3c7076a747ed780f98b9e6bd1f7c28035f9d5f
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-01-20 19:19:29 +0400

    test fix

--- data/courses.js
+++ data/courses.js
@@ -2,46 +2,46 @@ export const coursesData = [
   {
     name: "iOS Development",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/01.webp",
+    photo: "./../assets/img/courses/01.webp",
   },
   {
     name: "React",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/02.webp",
+    photo: "./../assets/img/courses/02.webp",
   },
   {
     name: "Intro to Python",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/03.webp",
+    photo: "./../assets/img/courses/03.webp",
   },
   {
     name: "Advanced Python",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/04.webp",
+    photo: "./../assets/img/courses/04.webp",
   },
   {
     name: "Advanced Cybersecurity Course",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/05.webp",
+    photo: "./../assets/img/courses/05.webp",
   },
   {
     name: "ToT - Training of Trainers",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/06.webp",
+    photo: "./../assets/img/courses/06.webp",
   },
   {
     name: "Blockchain",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/07.webp",
+    photo: "./../assets/img/courses/07.webp",
   },
   {
     name: "DevOps",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/08.webp",
+    photo: "./../assets/img/courses/08.webp",
   },
   {
     name: "Information Security Governance",
     reg: "რეგისტრაცია დასრულებულია",
-    photo: "../assets/img/courses/09.webp",
+    photo: "./../assets/img/courses/09.webp",
   }
 ]
