edd82206b97c36f9b816d2865fdeee562323b97e
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-01-20 21:36:40 +0400

    test fix added picture tag to use jpg format when webp is not supported

--- js/coursesRendering.js
+++ js/coursesRendering.js
@@ -5,10 +5,17 @@ const list = document.querySelector("#course_list");
 export const listRender = () => {
   coursesData.forEach(item => {
     const li = document.createElement("li");
+    const picture = document.createElement("picture");
+    const source = document.createElement("source");
+    source.srcset = item.webp;
+    source.type = "image/webp";
     const image = new Image();
-    image.src = item.photo;
+    image.src = item.jpg;
+    image.alt = "course";
+    picture.appendChild(source);
+    picture.appendChild(image);
+    li.appendChild(picture);
     list.appendChild(li);
-    li.appendChild(image);
   })
 }
 
