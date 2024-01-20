c6808caba98cd5a413836ebaef7d0550585a2951
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-01-20 23:46:55 +0400

    finished courses section

--- js/coursesRendering.js
+++ js/coursesRendering.js
@@ -2,19 +2,55 @@ import { coursesData } from "../data/courses.js";
 
 const list = document.querySelector("#course_list");
 
+const addImage = (href, alt) => {
+  const image = new Image();
+  image.src = href;
+  image.alt = alt;
+  return image;
+}
+
+const addPicture = (href1, href2, alt) => {
+  const picture = document.createElement("picture");
+  const source = document.createElement("source");
+  const image = addImage(href2, alt);
+  source.srcset = href1;
+  source.type = "image/webp";
+  picture.appendChild(source);
+  picture.appendChild(image);
+  return picture;
+}
+
+const addContent = (heading, paragraph, anchor) => {
+  const container = document.createElement("div");
+  container.className = "course_info";
+
+  const h5 = document.createElement("h5");
+  const p = document.createElement("p");
+  const a = document.createElement("a");
+  const icon = addImage("./assets/icons/rightArrowBlue.svg", "arrow");
+
+  const anchor_div = document.createElement("div");
+  anchor_div.className = "course_details"
+
+  h5.textContent = heading;
+  p.textContent = paragraph;
+  a.textContent = anchor;
+  a.href = "#";
+
+  anchor_div.append(icon, a);
+  container.append(h5, p, anchor_div);
+
+  return container;
+}
+
 export const listRender = () => {
   coursesData.forEach(item => {
     const li = document.createElement("li");
-    const picture = document.createElement("picture");
-    const source = document.createElement("source");
-    source.srcset = item.webp;
-    source.type = "image/webp";
-    const image = new Image();
-    image.src = item.jpg;
-    image.alt = "course";
-    picture.appendChild(source);
-    picture.appendChild(image);
-    li.appendChild(picture);
+
+    const picture = addPicture(item.webp, item.jpg, "course");
+    const course_container = addContent(item.name, item.reg, "კურსის დეტალები");
+
+    li.append(picture, course_container);
     list.appendChild(li);
   })
 }
